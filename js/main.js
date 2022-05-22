c4df7a5a13ac340da627baf648199d222c4c8683
mita-n <98956240+mita-n@users.noreply.github.com>
2022-05-22 18:33:41 +0900

    'add deletebutton'

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
         todos : [],
     }),
     methods : {
+        //アイテム追加のメソッド
         addItem : function (){
             //console.log("clicked!!!")
             //入力が空だったらリターンして処理をスキップする
@@ -17,7 +18,15 @@ const app = Vue.createApp({
                 this.todos.push(todo)
                 //addボタン押した際にテキストボックスを空にする
                 this.newItem = ""
+        },
+        //アイテム削除のメソッド 引数にhtmlのv-forで回しているindexを引いている
+        //indexは必ずしもプロパティ名をindexにしなくてもいい indekkusuでもいいし他でもいい
+        deleteItem : function (index){
+            console.log("Delete!!!")
+            console.log(index)
+            this.todos.splice(index,1)
         }
+
     },
 })
 app.mount("#app")
